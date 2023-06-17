186c7a147defe0be99f4805aaa4e716e75be25ed
robinNcode <msmrobin518@gmail.com>
2023-06-17 22:43:06 +0600

    update: signUp page onsubmit functionality added

--- frontend/src/views/Signup.jsx
+++ frontend/src/views/Signup.jsx
@@ -1,18 +1,49 @@
-import React from "react";
+import {React, useRef} from "react";
+import axiosClient from "../axios_client.js";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 export default function Signup() {
-  function onSubmit(evt) {
-    evt.preventDefault();
-    return undefined;
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  const {setCurrentUser, setToken} = useStateContext();
+  function onSubmit(e) {
+    e.preventDefault();
+
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      confirm_password: confirmPasswordRef.current.value
+    }
+
+    axiosClient.post("/signup", payload)
+      .then(({result}) => {
+        /**
+         * If the user is successfully created, we will set the current user and token
+         * automatically redirect to the home page ...
+         */
+        setCurrentUser(result.user);
+        setToken(result.token);
+      })
+      .catch((err) => {
+        const response = err.response;
+
+        if (response && response.status === 422) {
+          console.log(response.data.errors);
+        }
+      });
   }
 
   return (
     <form onSubmit={onSubmit}>
       <h1 className={"title"}>Awesome to see you!</h1>
-      <input type={"text"} name={"name"} placeholder={"Full Name"}/>
-      <input type={"email"} name={"email"} placeholder={"Email"}/>
-      <input type={"password"} name={"password"} placeholder={"Password"}/>
-      <input type={"password"} name={"password"} placeholder={"Confirm Password"}/>
+      <input ref={nameRef} type={"text"} name={"name"} placeholder={"Full Name"}/>
+      <input ref={emailRef} type={"email"} name={"email"} placeholder={"Email"}/>
+      <input ref={passwordRef} type={"password"} name={"password"} placeholder={"Password"}/>
+      <input ref={confirmPasswordRef} type={"password"} name={"password"} placeholder={"Confirm Password"}/>
 
       <button className={"btn btn-block"}>Sign Up</button>
 
