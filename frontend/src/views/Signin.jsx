c517af4eaef47d4005f9e13efbebe8e5e3fe1813
robinNcode <msmrobin518@gmail.com>
2023-08-09 20:37:53 +0600

    some updates

--- frontend/src/views/Signin.jsx
+++ frontend/src/views/Signin.jsx
@@ -1,11 +1,27 @@
-import React, {useRef, useState} from "react";
+import * as React from 'react';
+import {useRef, useState} from "react";
 import axiosClient from "../axios_client.js";
-import {useStateContext} from "../contexts/ContextProvider.jsx";
+import { useStateContext } from "../contexts/ContextProvider.jsx";
+
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {Copyright} from "@mui/icons-material";
 
 export default function Signin() {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const {setCurrentUser, setToken} = useStateContext();
+  const { setCurrentUser, setToken } = useStateContext();
 
   /**
    * To set errors
@@ -42,25 +58,82 @@ export default function Signin() {
   }
 
   return (
-    <form onSubmit={onSubmit}>
-      <h1 className={"title"}>Sign into your awesome App!</h1>
-
-      {
-        errors &&
-        Object.keys(errors).map((key) => {
-          return (
-            <p key={key} className={"alert"}>{errors[key][0]}</p>
-          );
-        })
-      }
-
-      <input ref={emailRef} type={"email"} name={"email"} placeholder={"Email"}/>
-      <input ref={passwordRef} type={"password"} name={"password"} placeholder={"Password"}/>
-      <button className={"btn btn-block"}>Sign In</button>
-
-      <p className={"message"}>
-        Not registered? <a href={"/signup"}>Create an account</a>
-      </p>
-    </form>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={onSubmit} noValidate sx={{ mt: 1 }}>
+              {
+                errors &&
+                Object.keys(errors).map((key) => {
+                  return (
+                    <p key={key} className={"alert"}>{errors[key][0]}</p>
+                  );
+                })
+              }
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              ref={emailRef}
+              name={"email"}
+              label={"Email"}
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              ref={passwordRef}
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  <a href={"/signup"}>Create an account</a>
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
   );
 }
